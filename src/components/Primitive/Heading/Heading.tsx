c321c820a70d59a90add2764081c535884b3428d
Mikai Somerville <91355759+mikais13@users.noreply.github.com>
2025-11-18 21:50:00 +1300

    feat(components): add `Heading` component (#34)
    * feat(deps): install tailwind variants
    
    * feat(components): add Heading component
    
    * feat(heading): use relevant h tag within heading component

---
+++ src/components/Primitive/Heading/Heading.tsx
@@ -0,0 +1,68 @@
+import type { JSX } from "react"
+import { cn } from "@/lib/utils"
+import { type HeadingVariants, headingVariants } from "./variants"
+
+/**
+ * Props for the Heading component.
+ */
+export interface HeadingProps {
+  /**
+   * Heading level (1-6).
+   */
+  h: HeadingVariants["h"]
+  /**
+   * Heading text content.
+   */
+  children: string
+  /**
+   * Whether to display a period at the end of the heading.
+   */
+  period?: boolean
+  /**
+   * Additional class names for the heading element.
+   */
+  className?: string
+  /**
+   * Additional class names for the period element.
+   */
+  periodClassName?: string
+}
+
+/**
+ * Heading component for displaying headings with optional period.
+ *
+ * @param h Heading level (1-6).
+ * @param children Heading text content.
+ * @param period Whether to display a period at the end of the heading.
+ * @param className Additional class names for the heading element.
+ * @param periodClassName Additional class names for the period element.
+ * @returns Component for rendering a heading.
+ * @example
+ * <Heading h={1}>Example Heading</Heading>
+ * <Heading h={2} period>Heading With Period</Heading>
+ */
+export const Heading = ({
+  h,
+  children,
+  period = false,
+  className,
+  periodClassName,
+}: HeadingProps) => {
+  const { textVariant, periodVariant } = headingVariants({ h })
+  const Element = `h${h}` as keyof JSX.IntrinsicElements
+  return (
+    <Element className={cn(className, textVariant())}>
+      {period ? (
+        <>
+          {children.substring(0, children.lastIndexOf(" ") + 1)}
+          <span className="whitespace-nowrap">
+            {children.substring(children.lastIndexOf(" ") + 1)}
+            {period && <span className={cn(periodVariant(), periodClassName)} />}
+          </span>
+        </>
+      ) : (
+        children
+      )}
+    </Element>
+  )
+}
