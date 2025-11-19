b03febfd266862a1abdb87d4ca5f894926de5c9f
mikais13 <mikai.somerville@gmail.com>
2025-11-20 12:35:41 +1300

    feat(components): add ValueFingerprint generic component

---
+++ src/components/Generic/ValuesFingerprint/variants.ts
@@ -0,0 +1,45 @@
+import { tv, type VariantProps } from "tailwind-variants"
+
+export const valuesFingerprintVariants = tv({
+  slots: {
+    svg: "relative",
+    container: "relative",
+    label: "-translate-y-1/2 absolute top-1/2 transform font-semibold",
+  },
+  variants: {
+    colour: {
+      purple: {
+        svg: "fill-accent-purple",
+      },
+      blue: {
+        svg: "fill-accent-blue",
+      },
+      green: {
+        svg: "fill-accent-green",
+      },
+      red: {
+        svg: "fill-accent-red",
+      },
+    },
+    size: {
+      sm: {
+        svg: "size-32",
+        label: "text-5xl ml-7",
+      },
+      md: {
+        svg: "size-38",
+        label: "text-6xl ml-8",
+      },
+      lg: {
+        svg: "size-44",
+        label: "text-7xl ml-9",
+      },
+    },
+  },
+  defaultVariants: {
+    colour: "purple",
+    size: "md",
+  },
+})
+
+export type ValuesFingerprintVariants = VariantProps<typeof valuesFingerprintVariants>
