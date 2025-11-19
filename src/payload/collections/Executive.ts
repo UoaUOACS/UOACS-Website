0992924019956bf91098a51f94faff9be5218503
Mikai Somerville <91355759+mikais13@users.noreply.github.com>
2025-11-19 21:27:53 +1300

    feat(payload): add `Executive` payload collection (#28)
    * feat(payload): add Executive collection
    
    * refactor(payload): use enum for exec team
    
    * revert(payload): remove accidental index

---
+++ src/payload/collections/Executive.ts
@@ -0,0 +1,74 @@
+import type { CollectionConfig } from "payload"
+import { ExecutiveTeam } from "@/types/enums"
+
+export const Executive: CollectionConfig = {
+  slug: "executive",
+  access: {
+    read: () => true,
+  },
+  admin: {
+    useAsTitle: "name",
+  },
+  fields: [
+    {
+      name: "name",
+      type: "text",
+      required: true,
+      admin: {
+        description: "Full name of the executive member",
+      },
+    },
+    {
+      name: "isCurrent",
+      type: "checkbox",
+      label: "Is Current Member?",
+      defaultValue: true,
+      required: true,
+      admin: {
+        description: "Indicates if the member is currently serving in the executive team",
+      },
+    },
+    {
+      name: "role",
+      type: "group",
+      label: "Role Details",
+      fields: [
+        {
+          name: "title",
+          type: "text",
+          required: false,
+          admin: {
+            description:
+              "Title of the executive role (e.g., Co-President, Treasurer). Should be required if a current member.",
+          },
+        },
+        {
+          name: "team",
+          type: "select",
+          options: Object.values(ExecutiveTeam),
+          required: true,
+          admin: {
+            description: "Team the executive member belongs to",
+          },
+        },
+      ],
+    },
+    {
+      name: "photo",
+      type: "relationship",
+      relationTo: "media",
+      required: false,
+      admin: {
+        description: "Photo of the executive member (required if a current member)",
+      },
+    },
+    {
+      name: "linkedin",
+      type: "text",
+      required: false,
+      admin: {
+        description: "LinkedIn profile URL of the executive member",
+      },
+    },
+  ],
+}
