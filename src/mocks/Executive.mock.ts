4292a4bd08eb45724fb847aaa1f288d4c808a94f
mikais13 <mikai.somerville@gmail.com>
2025-11-21 10:20:23 +1300

    feat(mocks): add mock Executive

---
+++ src/mocks/Executive.mock.ts
@@ -0,0 +1,22 @@
+import type { Executive } from "@/payload/payload-types"
+
+export const mockExecutive: Executive = {
+  id: "68e390f21023fb09c6a454da",
+  name: "Joshua Li",
+  isCurrent: true,
+  role: {
+    title: "Senior Advisor",
+    team: "admin",
+  },
+  photo: {
+    id: "media123",
+    url: "media/JoshLi.jpeg",
+    alt: "Joshua Li Profile Photo",
+    width: 500,
+    height: 500,
+    createdAt: "2024-05-01T12:00:00Z",
+    updatedAt: "2024-05-01T12:00:00Z",
+  },
+  updatedAt: "2025-05-01T12:00:00Z",
+  createdAt: "2024-05-01T12:00:00Z",
+}
