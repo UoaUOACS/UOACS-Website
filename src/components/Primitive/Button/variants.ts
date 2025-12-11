48cfa8f1c2c5312f5398274ef29d000df7e51c2d
Mikai Somerville <91355759+mikais13@users.noreply.github.com>
2025-12-11 18:01:30 +1300

    feat(primitives): add `Button` primitive component (#33)
    * feat(tailwind): configure theme variables
    
    * chore(storybook): link tailwind config to stories
    
    * chore(app): remove placeholder styles
    
    * chore(lint): perform lint fix
    
    * feat(deps): install fontawesome icons
    
    * feat(deps): install tailwind variants
    
    * feat(primitives): add button component
    
    * docs(button): add jsdoc comments to the button component
    
    * refactor(button): use html button attributes
    
    * chore(components): use primitive folder in stories
    
    * feat(button): increase accessability
    
    * feat(button): enhance disabled state styles

---
+++ src/components/Primitive/Button/variants.ts
@@ -0,0 +1,74 @@
+import { tv, type VariantProps } from "tailwind-variants"
+
+/**
+ * Button variant configurations.
+ * Consists of themes, sizes, and border options.
+ */
+export const buttonVariants = tv({
+  slots: {
+    inner:
+      "flex flex-row gap-4 items-center rounded-xl border-4 border-transparent bg-black p-4 font-mono text-white text-left",
+    border:
+      "rounded-2xl border-4 border-transparent cursor-pointer focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black disabled:cursor-not-allowed disabled:opacity-50",
+  },
+  variants: {
+    theme: {
+      primary: {
+        inner: "bg-primary",
+        border: "border-primary-border",
+      },
+      secondary: {
+        inner: "bg-secondary",
+        border: "",
+      },
+      ghost: {
+        inner: "bg-transparent text-black",
+        border: "border-gray-300",
+      },
+      dark: {
+        inner: "bg-black",
+        border: "border-gray-300",
+      },
+      light: {
+        inner: "bg-gray-300 text-black",
+        border: "border-gray-200",
+      },
+    },
+    size: {
+      sm: {
+        inner: "text-sm px-4 py-2 gap-2",
+      },
+      "sm-wide": {
+        inner: "text-sm px-6 py-2 gap-2",
+      },
+      md: {
+        inner: "text-md p-4 gap-4",
+      },
+      "md-wide": {
+        inner: "text-md px-6 py-4 gap-4",
+      },
+      lg: {
+        inner: "text-lg p-6 gap-6",
+      },
+      "lg-wide": {
+        inner: "text-lg px-10 py-6 gap-6",
+      },
+    },
+    border: {
+      true: {},
+      false: {
+        border: "border-transparent",
+      },
+    },
+  },
+  defaultVariants: {
+    theme: "primary",
+    size: "md",
+    border: true,
+  },
+})
+
+/**
+ * Props for the button variant configuration.
+ */
+export type ButtonVariantProps = VariantProps<typeof buttonVariants>
