274795062182593c9d5af5943920859d4d56fb44
mikais13 <mikai.somerville@gmail.com>
2026-01-04 21:50:42 +1300

    fix: use nextjs local font to preload switzer font

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,10 +1,17 @@
 import { Inter_Tight } from "next/font/google"
+import localFont from "next/font/local"
 import type React from "react"
 import "../globals.css"
 
 const inter = Inter_Tight({
   subsets: ["latin"],
   variable: "--font-inter",
+})
+
+const switzer = localFont({
+  src: "../../../public/fonts/Switzer-Variable.woff2",
+  variable: "--font-switzer",
+  display: "swap",
 })
 
 export const metadata = {
@@ -16,7 +23,7 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
-    <html className={inter.variable} lang="en">
+    <html className={`${inter.variable} ${switzer.variable}`} lang="en">
       <body>
         <main>{children}</main>
       </body>
