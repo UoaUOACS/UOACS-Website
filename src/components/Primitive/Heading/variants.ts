a7fb3e88dd37b9293d9172beb04b2e4ac703fc4a
mikais13 <mikai.somerville@gmail.com>
2026-01-05 17:29:54 +1300

    feat(heading): increase period gap and improve responsiveness

--- src/components/Primitive/Heading/variants.ts
+++ src/components/Primitive/Heading/variants.ts
@@ -6,17 +6,35 @@ import { tv, type VariantProps } from "tailwind-variants"
 export const headingVariants = tv({
   slots: {
     textVariant:
-      "flex flex-row flex-wrap items-end justify-start font-semibold gap-x-[0.2em] text-base font-inter",
-    periodVariant: "ml-0.5 bg-primary inline-block",
+      "flex flex-row flex-wrap items-end justify-center font-semibold gap-x-[0.2em] text-base font-inter text-center",
+    periodVariant: "bg-primary inline-block",
   },
   variants: {
     h: {
-      1: { textVariant: "text-9xl", periodVariant: "size-3 rounded-xs" },
-      2: { textVariant: "text-8xl", periodVariant: "size-2.5 rounded-xs" },
-      3: { textVariant: "text-6xl", periodVariant: "size-2 rounded-xs" },
-      4: { textVariant: "text-4xl", periodVariant: "size-1.5 rounded-xs" },
-      5: { textVariant: "text-3xl", periodVariant: "size-1 rounded-2xs" },
-      6: { textVariant: "text-2xl", periodVariant: "size-1 rounded-2xs" },
+      1: {
+        textVariant: "text-7xl md:text-9xl",
+        periodVariant: "size-2 ml-1.5 md:size-3 md:ml-3 rounded-xs",
+      },
+      2: {
+        textVariant: "text-6xl md:text-8xl",
+        periodVariant: "size-1.5 ml-1 md:size-2.5 md:ml-2.5 rounded-xs",
+      },
+      3: {
+        textVariant: "text-5xl md:text-6xl",
+        periodVariant: "size-1.5 ml-1 md:size-2 md:ml-2 rounded-xs",
+      },
+      4: {
+        textVariant: "text-3xl md:text-4xl",
+        periodVariant: "size-1 ml-0.5 md:size-1.5 md:ml-1.5 rounded-xs",
+      },
+      5: {
+        textVariant: "text-2xl md:text-3xl",
+        periodVariant: "size-1 ml-0.5 md:ml-1 rounded-2xs",
+      },
+      6: {
+        textVariant: "text-xl md:text-2xl",
+        periodVariant: "size-1 ml-0.5 md:ml-1 rounded-2xs",
+      },
     },
   },
   defaultVariants: {
