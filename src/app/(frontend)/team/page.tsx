783909897601eb7e1a4ceb8691a730c4b1dc235d
mikais13 <mikai.somerville@gmail.com>
2026-01-05 21:50:49 +1300

    fix(team): remove repeated caching

--- src/app/(frontend)/team/page.tsx
+++ src/app/(frontend)/team/page.tsx
@@ -3,8 +3,6 @@ import { TeamPageClient } from "./__components__/TeamPageClient"
 
 const CACHE_TIME = 3600 // 1 hour
 
-export const revalidate = CACHE_TIME
-
 export default async function TeamPage() {
   const execs: { docs: Executive[] } = await fetch(
     `${process.env.NEXT_PUBLIC_URL}/payload/api/executive`,
