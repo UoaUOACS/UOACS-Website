7c0ea2133d0f44aa2bd7913491e5eb76745b71b5
mikais13 <mikai.somerville@gmail.com>
2026-01-07 00:10:55 +1300

    feat(layout): add navbar and footer to page layout

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,6 +1,10 @@
 import { Inter_Tight } from "next/font/google"
 import type React from "react"
 import "../globals.css"
+import { faDiscord, faInstagram, faLinkedin, faTiktok } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Footer, Navbar } from "@/components/Composite"
+import type { DropdownOptionProps } from "@/components/Primitive/Dropdown/DropdownOption"
 
 const inter = Inter_Tight({
   subsets: ["latin"],
@@ -12,13 +16,56 @@ export const metadata = {
   title: "Payload Blank Template",
 }
 
+const navbarLinks: { label: string; href: string }[] = [
+  { label: "MEET THE TEAM", href: "/team" },
+  { label: "OUR SPONSORS", href: "/sponsors" },
+]
+const navbarSocialLinks: DropdownOptionProps[] = [
+  {
+    label: (
+      <span className="flex items-center gap-2">
+        <FontAwesomeIcon className="w-4" icon={faDiscord} /> Discord
+      </span>
+    ),
+    href: "https://discord.gg/xSgqAmGE",
+  },
+  {
+    label: (
+      <span className="flex items-center gap-2">
+        <FontAwesomeIcon className="w-4" icon={faInstagram} /> Instagram
+      </span>
+    ),
+    href: "https://www.instagram.com/uoacs25/",
+  },
+  {
+    label: (
+      <span className="flex items-center gap-2">
+        <FontAwesomeIcon className="w-4" icon={faTiktok} /> TikTok
+      </span>
+    ),
+    href: "https://www.tiktok.com/@uoacs?lang=en-GB",
+  },
+  {
+    label: (
+      <span className="flex items-center gap-2">
+        <FontAwesomeIcon className="w-4" icon={faLinkedin} /> LinkedIn
+      </span>
+    ),
+    href: "https://www.linkedin.com/company/university-of-auckland-compsci-society/posts/?feedView=all",
+  },
+]
+
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
     <html className={inter.variable} lang="en">
       <body>
-        <main>{children}</main>
+        <div className="flex flex-col gap-9 px-16 py-6">
+          <Navbar links={navbarLinks} socialLinks={navbarSocialLinks} />
+          <main className="flex flex-col items-center gap-9 py-9 md:gap-18">{children}</main>
+        </div>
+        <Footer />
       </body>
     </html>
   )
