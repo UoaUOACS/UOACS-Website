8e09a36deb42d3975df8cf0b94f303f7b1498290
mikais13 <mikai.somerville@gmail.com>
2026-01-08 14:11:11 +1300

    feat(components): add LazyImage component

---
+++ src/components/Primitive/LazyImage/LazyImage.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { LazyImage } from "./LazyImage"
+
+const meta: Meta<typeof LazyImage> = {
+  title: "Primitive Components/LazyImage",
+  component: LazyImage,
+  argTypes: {
+    src: { control: "text" },
+    alt: { control: "text" },
+    width: { control: "number" },
+    height: { control: "number" },
+    className: { control: "text" },
+  },
+  args: {
+    src: "https://placehold.co/600x400",
+    alt: "Placeholder Image",
+    width: 600,
+    height: 400,
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof LazyImage>
+
+export const Primary: Story = {}
