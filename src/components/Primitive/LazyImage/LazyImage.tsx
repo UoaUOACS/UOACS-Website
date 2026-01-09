cb5561abdf78a45e925d87d0a2aa5c49103710b5
Mikai Somerville <91355759+mikais13@users.noreply.github.com>
2026-01-10 09:52:01 +1300

    feat(components): add LazyImage component (#68)
    * feat(components): add LazyImage component
    
    * fix(lazy-image): use onLoadingComplete and onError to ensure loading is always completed
    
    * chore(lazy-image): add skeletonClassName to jsdoc
    
    * feat(lazy-image): add aria-live to skeleton div

---
+++ src/components/Primitive/LazyImage/LazyImage.tsx
@@ -0,0 +1,67 @@
+"use client"
+
+import Image, { type ImageProps } from "next/image"
+import { useState } from "react"
+import { cn } from "@/lib/utils"
+
+/**
+ * A component that displays an image with a loading placeholder.
+ */
+interface LazyImageProps extends ImageProps {
+  /**
+   * The class name for the container div
+   */
+  containerClassName?: string
+  /**
+   * The class name for the skeleton placeholder
+   */
+  skeletonClassName?: string
+}
+
+/**
+ * A component that displays an image with a loading placeholder.
+ *
+ * @param width The width of the image
+ * @param height The height of the image
+ * @param className The class name for the image
+ * @param containerClassName The class name for the container div
+ * @param skeletonClassName The class name for the skeleton placeholder
+ * @param props Other {@link ImageProps}
+ */
+export function LazyImage({
+  width,
+  height,
+  className,
+  containerClassName,
+  skeletonClassName,
+  ...props
+}: LazyImageProps) {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  return (
+    <div className={cn("relative", containerClassName)} style={{ width, height }}>
+      {!isLoaded && (
+        <div
+          aria-live="polite"
+          className={cn("absolute inset-0 animate-pulse rounded-lg bg-gray-200", skeletonClassName)}
+        />
+      )}
+      <Image
+        className={cn(
+          "h-full w-full object-contain transition-opacity duration-300",
+          isLoaded ? "opacity-100" : "opacity-0",
+          className,
+        )}
+        height={height}
+        onError={() => {
+          setIsLoaded(true)
+        }}
+        onLoadingComplete={() => {
+          setIsLoaded(true)
+        }}
+        width={width}
+        {...props}
+      />
+    </div>
+  )
+}
--- src/components/Primitive/index.ts
+++ src/components/Primitive/index.ts
@@ -1,3 +1,4 @@
 export * from "./Button/Button"
 export * from "./Dropdown/Dropdown"
 export * from "./Heading/Heading"
+export * from "./LazyImage/LazyImage"
