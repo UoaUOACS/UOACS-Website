7e71ba808251593742e903159e01151725764449
mikais13 <mikai.somerville@gmail.com>
2026-01-20 23:22:42 +1300

    fix(navbar): use new socialLinks format in stories

--- src/components/Composite/Navbar/Navbar.stories.tsx
+++ src/components/Composite/Navbar/Navbar.stories.tsx
@@ -1,5 +1,4 @@
 import { faDiscord, faInstagram, faLinkedin, faTiktok } from "@fortawesome/free-brands-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import type { Meta, StoryObj } from "@storybook/react"
 import { Navbar } from "./Navbar"
 
@@ -8,36 +7,12 @@ const meta: Meta<typeof Navbar> = {
   component: Navbar,
   args: {
     socialLinks: [
+      { label: "Discord", icon: faDiscord, href: "https://discord.gg/xSgqAmGE" },
+      { label: "Instagram", icon: faInstagram, href: "https://www.instagram.com/uoacs25/" },
+      { label: "TikTok", icon: faTiktok, href: "https://www.tiktok.com/@uoacs?lang=en-GB" },
       {
-        label: (
-          <>
-            <FontAwesomeIcon icon={faDiscord} /> Discord
-          </>
-        ),
-        href: "https://discord.gg/xSgqAmGE",
-      },
-      {
-        label: (
-          <>
-            <FontAwesomeIcon icon={faInstagram} /> Instagram
-          </>
-        ),
-        href: "https://www.instagram.com/uoacs25/",
-      },
-      {
-        label: (
-          <>
-            <FontAwesomeIcon icon={faTiktok} /> TikTok
-          </>
-        ),
-        href: "https://www.tiktok.com/@uoacs?lang=en-GB",
-      },
-      {
-        label: (
-          <>
-            <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
-          </>
-        ),
+        label: "LinkedIn",
+        icon: faLinkedin,
         href: "https://www.linkedin.com/company/university-of-auckland-compsci-society/posts/?feedView=all",
       },
     ],
