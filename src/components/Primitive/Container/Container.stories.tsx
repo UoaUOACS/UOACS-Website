ce809634a1626647b745a17b2bab8c19a353e14e
mikais13 <mikai.somerville@gmail.com>
2026-02-01 00:37:10 +1300

    feat(components): add Container component

---
+++ src/components/Primitive/Container/Container.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { Container } from "./Container"
+import { containerVariants } from "./Container.variants"
+
+const meta: Meta<typeof Container> = {
+  title: "Primitive Components/Container",
+  component: Container,
+  argTypes: {
+    children: { control: false },
+    theme: {
+      control: { type: "select" },
+      options: containerVariants.variants.theme
+        ? Object.keys(containerVariants.variants.theme)
+        : [],
+    },
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof Container>
+
+export const Default: Story = {
+  args: {
+    children: <div className="px-6 py-6">This is a container</div>,
+    title: "Container Title",
+  },
+}
