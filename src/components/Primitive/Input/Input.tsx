4f66b2f54aa4a3c04f46d92c458fc6e202f9df12
mikais13 <mikai.somerville@gmail.com>
2026-02-28 00:24:55 +1300

    feat(components): add Input component

--- src/components/Primitive/Input/Input.tsx
+++ src/components/Primitive/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useId } from "react"
+import { forwardRef } from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps extends React.ComponentPropsWithoutRef<"input"> {
@@ -9,16 +9,15 @@ export interface InputProps extends React.ComponentPropsWithoutRef<"input"> {
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ label, error, containerClassName, className, required, ...props }, ref) => {
-    const id = useId()
     return (
       <div className={cn("flex w-full flex-col justify-start gap-1 font-mono", containerClassName)}>
-        <label className="block font-medium text-gray-700 text-sm" htmlFor={id}>
+        <label className="block font-medium text-gray-700 text-sm" htmlFor={label}>
           {label}
           {required && <span className="ml-1 text-brand-pink">*</span>}
         </label>
         <input
           className={cn("w-full rounded border border-gray-300 px-3 py-2", className)}
-          id={id}
+          id={label}
           ref={ref}
           {...props}
         />
