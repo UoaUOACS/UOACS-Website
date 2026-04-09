b8490027941a3c33e898db4f29302be8702bc1b3
mikais13 <mikai.somerville@gmail.com>
2026-04-10 00:08:02 +1200

    feat(schemas): add better auth related schemas

---
+++ src/types/schemas/user.ts
@@ -0,0 +1,20 @@
+import type { User } from "better-auth"
+import { z } from "zod"
+
+export const createUserSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.email({ error: "Please enter a valid email" }),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+}) satisfies z.ZodType<Partial<User>>
+
+export type CreateUserInput = z.infer<typeof createUserSchema>
+
+export const userSchema = z.object({
+  id: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  email: z.email(),
+  emailVerified: z.boolean(),
+  name: z.string().min(2).max(100),
+  image: z.url().optional().nullable(),
+}) satisfies z.ZodType<User>
