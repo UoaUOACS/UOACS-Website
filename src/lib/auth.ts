4605e7babf0b96608ff9711f3a545242b8f69ddb
mikais13 <mikai.somerville@gmail.com>
2026-04-09 15:01:25 +1200

    chore: replace BETTER_AUTH_URL with equivalent NEXT_PUBLIC_URL

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,13 +2,13 @@ import { betterAuth } from "better-auth"
 import { mongodbAdapter } from "better-auth/adapters/mongodb"
 import { mongoClient } from "./mongo"
 
-if (!process.env.BETTER_AUTH_SECRET || !process.env.BETTER_AUTH_URL) {
-  throw new Error("BETTER_AUTH_SECRET and BETTER_AUTH_URL environment variables are not set")
+if (!process.env.BETTER_AUTH_SECRET || !process.env.NEXT_PUBLIC_URL) {
+  throw new Error("BETTER_AUTH_SECRET and NEXT_PUBLIC_URL environment variables are not set")
 }
 
 export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
-  url: process.env.BETTER_AUTH_URL,
+  url: process.env.NEXT_PUBLIC_URL,
   adapter: mongodbAdapter(mongoClient.db()),
   emailAndPassword: {
     enabled: true,
