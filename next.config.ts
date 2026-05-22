857a6b2a58db9cebdbb5c16819af1e809f0c65c8
mikais13 <mikai.somerville@gmail.com>
2026-05-22 12:28:54 +1200

    fix(next): add media api routes to localPatterns to hush new error

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "standalone",
+  images: {
+    localPatterns: [
+      {
+        pathname: "/payload/api/media/file/**",
+      },
+    ],
+  },
   turbopack: {
     resolveExtensions: [".mdx", ".tsx", ".ts", ".jsx", ".js", ".mjs", ".json"],
     rules: {
