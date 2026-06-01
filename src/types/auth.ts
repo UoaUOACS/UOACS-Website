e32ede66b75916397d3f0e405cc967bf8f33a75c
mikais13 <mikai.somerville@gmail.com>
2026-06-02 00:25:42 +1200

    chore: remove accidental commited auth type file

--- src/types/auth.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod"
-import { memberSchema } from "./schemas/member"
-import { userSchema } from "./schemas/user"
-
-export const memberWithAuthSchema = z.union([memberSchema, userSchema])
-
-export type MemberWithAuth = z.infer<typeof memberWithAuthSchema>
