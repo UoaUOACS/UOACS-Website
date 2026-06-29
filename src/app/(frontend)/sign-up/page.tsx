626c5f40a82c7f074fa8c88294c799a13001cb74
mikais13 <mikai.somerville@gmail.com>
2026-06-30 00:49:08 +1200

    feat(signin): use the GuardOnly wrapper

--- src/app/(frontend)/sign-up/page.tsx
+++ src/app/(frontend)/sign-up/page.tsx
@@ -2,22 +2,25 @@ import Link from "next/link"
 import { SignUpForm } from "@/components/Composite"
 import { Heading } from "@/components/Primitive"
 import { Routes } from "@/lib/routes"
+import { GuestOnly } from "../_components/GuestOnly"
 
 export default function SignUpPage() {
   return (
-    <div className="flex w-full flex-col justify-center gap-8 px-4">
-      <Heading className="justify-start" h={3}>
-        Sign Up
-      </Heading>
-      <div className="flex w-full flex-col justify-center gap-4">
-        <SignUpForm />
-        <p className="paragraph-xs text-gray-500">
-          Already Signed Up?{" "}
-          <Link className="underline transition-colors hover:text-gray-700" href={Routes.LOGIN}>
-            Log In
-          </Link>
-        </p>
+    <GuestOnly>
+      <div className="flex w-full flex-col justify-center gap-8 px-4">
+        <Heading className="justify-start" h={3}>
+          Sign Up
+        </Heading>
+        <div className="flex w-full flex-col justify-center gap-4">
+          <SignUpForm />
+          <p className="paragraph-xs text-gray-500">
+            Already Signed Up?{" "}
+            <Link className="underline transition-colors hover:text-gray-700" href={Routes.LOGIN}>
+              Log In
+            </Link>
+          </p>
+        </div>
       </div>
-    </div>
+    </GuestOnly>
   )
 }
